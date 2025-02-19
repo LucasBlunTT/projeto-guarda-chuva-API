c8763230639616581a9f5f8ddb1189211ce646e2
Lucas da Silva <sklucassilva@gmail.com>
2025-02-19 19:50:20 -0300

    feat: Adicionar middleware de verificação de administrador ou motorista e aplicar nas rotas de usuário

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
 import verifyAuthentication from '../middlewares/verifyAuthentication';
+import verifyAdminOrDriver from '../middlewares/verifyAdminOrDriver';
 
 const userController = new UserController();
 const userRoutes = Router();
 
-userRoutes.get('/', verifyAuthentication, userController.getAll);
-userRoutes.get('/:id', verifyAuthentication, userController.getById);
-userRoutes.post('/', verifyAuthentication, userController.create);
-userRoutes.put('/:id', verifyAuthentication, userController.update);
+userRoutes.get('/', verifyAuthentication, verifyAdminOrDriver, userController.getAll);
+userRoutes.get('/:id', verifyAuthentication, verifyAdminOrDriver, userController.getById);
+userRoutes.post('/', verifyAuthentication, verifyAdminOrDriver, userController.create);
+userRoutes.put('/:id', verifyAuthentication, verifyAdminOrDriver, userController.update);
 
 export default userRoutes;
