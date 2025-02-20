97d403dd17ed181de9b56896ebff1352d4d0051e
Lucas da Silva <sklucassilva@gmail.com>
2025-02-20 18:24:32 -0300

    feat: Criar tabela de produtos e entidade correspondente

---
+++ src/entities/Product.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import Branch from './Branch';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 200 })
+  name: string;
+
+  @Column({ type: 'int' })
+  amount: number;
+
+  @Column({ type: 'varchar', length: 200 })
+  description: string;
+
+  @Column({ type: 'varchar', length: 200, nullable: true })
+  url_cover: string;
+
+  @ManyToOne(() => Branch)
+  @JoinColumn({ name: 'branch_id' })
+  branch: Branch;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+}
