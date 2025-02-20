1ebd15164766938094e3a29564ce2d14392d579b
Lucas da Silva <sklucassilva@gmail.com>
2025-02-20 20:57:36 -0300

    feat: Adicionar funcionalidade para listar produtos com autenticação e verificação de filial

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -4,23 +4,23 @@ import Branch from '../entities/Branch';
 
 class ProductService {
   async create(data: any, userId: number) {
-    const branchRepository = AppDataSource.getRepository(Branch);
     const productRepository = AppDataSource.getRepository(Product);
 
-    const currentBranch = await branchRepository.findOne({
-      where: { user_id: userId },
-    });
-
-    if (!currentBranch) {
-      throw new Error('Filial não encontrada para o usuário');
-    }
-
     const product = productRepository.create({
-      branch: currentBranch,
       ...data,
+      branch: { user_id: userId } as Branch, // Relaciona o produto com a filial usando o user_id
     });
 
     return await productRepository.save(product);
+  }
+
+  async getAll(userId: number) {
+    const productRepository = AppDataSource.getRepository(Product);
+
+    return await productRepository.find({
+      where: { branch: { user_id: userId } },
+      relations: ['branch'], // Inclui a relação com a filial
+    });
   }
 }
 
